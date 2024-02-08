80383ef2c71cc00be686d59a83f52a4160b59bb7
MariamMohammed-20194798 <20194798@fue.edu.eg>
2024-02-08 21:18:58 +0200

    p

--- server/src/interfaces/customRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from "express";
-import { IUser } from "../models/userModel";
-export interface CustomRequest<T = Request["body"]> extends Request {
-  body: T;
-  user?: IUser;
-}
--- server/src/interfaces/userController.ts
+++
@@ -1,7 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-
-export interface DecodedToken extends JwtPayload {
-  id: string;
-  iat: number;
-  exp: number;
-}
